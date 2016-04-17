4292fc941dd51546ca0ba95ba81503bc8168a8f3
Stephen Castro <stevo@ip-172-31-1-161.us-west-1.compute.internal>
2016-04-17 23:17:04 +0000

    search now also works with searching star first and last name

--- src/main/webapp/WEB-INF/sources/fabflix/beans/Movie.java
+++ src/main/webapp/WEB-INF/sources/fabflix/beans/Movie.java
@@ -113,49 +113,107 @@ public class Movie implements Serializable {
 
               String finalQuery = "SELECT * FROM  movieView;";
 
-              // Declare our statement
-              PreparedStatement statement = connection.prepareStatement(starView);
+              //create array for first_name last_name permuations
+              String names[] = keywords.split("\\s+");
+              ArrayList<String> possibleFirstNames = new ArrayList<String>();
+              ArrayList<String> possibleLastNames = new ArrayList<String>();
+              //to prevent duplicate search results
+              HashSet<Integer> movieSet = new HashSet<Integer>();
 
-              statement.setString(1, "%" + keywords + "%");
-              statement.setString(2, "%" + keywords + "%");
-              statement.execute();
+              // Building the different permutations of
+              // first and last names when many names seperated by a space are entered
+              if(names.length > 1){
+                  StringBuilder firstName = new StringBuilder();
+                  StringBuilder lastName = new StringBuilder();
+                  for(int i = 0; i < names.length - 1; ++i){
+                      firstName.append(names[i]);
+                      firstName.append(" ");
+                      possibleFirstNames.add(firstName.substring(0, firstName.length()-1));
 
-              statement = connection.prepareStatement(movieView);
-
-              statement.setString(1, "%" + keywords + "%");
-              statement.setString(2, "%" + keywords + "%");
-              statement.setString(3, "%" + keywords + "%");
-              statement.execute();
-
-              Statement finalStatement = connection.createStatement();
-              ResultSet results = finalStatement.executeQuery(finalQuery);
-
-              // Iterate through each row of results
-              while (results.next())
-              {
-                Integer id = results.getInt("id");
-                String title = results.getString("title");
-                Integer year = results.getInt("year");
-                String director = results.getString("director");
-                String bannerUrl = results.getString("banner_url");
-                String trailerUrl = results.getString("trailer_url");
-
-                //Create movie object
-                movie = new MovieInfo(id, title, year, director, bannerUrl, trailerUrl, new ArrayList<Star>(), new ArrayList<String>());
-
-                //Add to movie list
-                searchResults.add(movie);
+                      for(int j = i + 1; j < names.length; ++j){
+                          if(j != names.length){
+                              lastName.append(names[j]);
+                              lastName.append(" ");
+                          }
+                      }
+                      possibleLastNames.add(lastName.substring(0, lastName.length()-1));
+                      lastName.setLength(0);
+                  }
               }
+              PreparedStatement statement = null;
+              Statement finalStatement = null;
+              ResultSet results = null;
+              //for iterating do while, stop when finished iterating through all permutations
+              int firstNameIndex = -1;
+
+              do{
+                  //always use firstname OR lastname with all keywords for first iteration
+                  if(firstNameIndex == -1){
+                    // Declare our statement
+                    statement = connection.prepareStatement(starView);
+
+                    statement.setString(1, "%" + keywords + "%");
+                    statement.setString(2, "%" + keywords + "%");
+                    statement.execute();
+                  }
+                  //for subsequent iterations, use permutations of first and last names ANDed together
+                  else{
+                    starView = "CREATE OR REPLACE VIEW starView AS " + 
+                    "SELECT * " + 
+                    "FROM stars AS s2 " + 
+                    "WHERE first_name LIKE ? AND last_name LIKE ?;";
+
+                    statement = connection.prepareStatement(starView);
+
+                    statement.setString(1, "%" + possibleFirstNames.get(firstNameIndex) + "%");
+                    statement.setString(2, "%" + possibleLastNames.get(firstNameIndex) + "%");
+                    statement.execute();
+
+                  }
+
+                  statement = connection.prepareStatement(movieView);
+
+                  statement.setString(1, "%" + keywords + "%");
+                  statement.setString(2, "%" + keywords + "%");
+                  statement.setString(3, "%" + keywords + "%");
+                  statement.execute();
+
+                  finalStatement = connection.createStatement();
+                  results = finalStatement.executeQuery(finalQuery);
+
+                  // Iterate through each row of results
+                  while (results.next())
+                  {
+                    Integer id = results.getInt("id");
+                    //proceed only if we haven't seen this movie yet
+                    if(movieSet.add(id)){
+                      String title = results.getString("title");
+                      Integer year = results.getInt("year");
+                      String director = results.getString("director");
+                      String bannerUrl = results.getString("banner_url");
+                      String trailerUrl = results.getString("trailer_url");
+
+                      //Create movie object
+                      movie = new MovieInfo(id, title, year, director, bannerUrl, trailerUrl, new ArrayList<Star>(), new ArrayList<String>());
+
+                      //Add to movie list
+                      searchResults.add(movie);
+                    }
+                  }
+                  ++firstNameIndex;
+              }while(firstNameIndex != possibleFirstNames.size());
+              
 
               results.close();
               statement.close();
+              finalStatement.close();
               connection.close();
 
         } 
         catch (Exception e) {
           e.printStackTrace();
         }
-      }
+      }// end if(!keywords.isEmpty())
         return searchResults;
   }
 }
