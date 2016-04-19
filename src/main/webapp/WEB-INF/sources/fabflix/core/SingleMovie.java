5cfd762140df5e082e472fb23e8fe542fb9dec91
Chris Ward <ChrisWard42@gmail.com>
2016-04-18 19:32:39 -0700

    Added single movie page functionality.

--- src/main/webapp/WEB-INF/sources/fabflix/core/SingleMovie.java
+++ src/main/webapp/WEB-INF/sources/fabflix/core/SingleMovie.java
@@ -7,6 +7,7 @@ import java.net.*;
 import java.sql.*;
 import java.text.*;
 import java.util.*;
+import java.util.Date;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import fabflix.beans.*;
@@ -20,6 +21,27 @@ public class SingleMovie extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
     {
+        // Get the movie id from the path info
+        String movieId = request.getPathInfo().toString().substring(1);
+
+        // TEMPORARY TEST CODE
+        Star star = new Star(420420, "Max", "Ushkalov", new Date(420, 4, 20)
+            , "https://lh6.googleusercontent.com/-X0MlOK7eabU/AAAAAAAAAAI/AAAAAAAABTs/3_u0TpKa4V0/s0/photo.jpg");
+        HashSet<Star> stars = new HashSet<Star>();
+        stars.add(star);
+        String genre = "Book of Manners";
+        HashSet<String> genres = new HashSet<String>();
+        genres.add(genre);
+        MovieInfo movie = new MovieInfo(420420, "Yolo Blaze It", 420, "Max Ushkalov"
+            , "https://lh6.googleusercontent.com/-X0MlOK7eabU/AAAAAAAAAAI/AAAAAAAABTs/3_u0TpKa4V0/s0/photo.jpg"
+            , "https://lh6.googleusercontent.com/-X0MlOK7eabU/AAAAAAAAAAI/AAAAAAAABTs/3_u0TpKa4V0/s0/photo.jpg"
+            , stars, genres);
+        // TEMPORARY TEST CODE
+
+        // TODO: Implement search query to get a single movie by its id
+        //MovieInfo movie = Movie.searchMovies(movieId);
+        request.setAttribute("movie", movie);
+        
         request.getRequestDispatcher("/WEB-INF/movie.jsp").forward(request, response);
     }
 
@@ -27,6 +49,6 @@ public class SingleMovie extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
     {
-       doGet(request, response);
+        doGet(request, response);
     }
 }
